37eff2ed1bb903e3f2b6125c1dadbdef8d8c1389
antonabr22 <antonabramow22@gmail.com>
2021-02-01 13:27:08 +0300

    Added Model and Mock Repository

---
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class MockCommandAPIRepo : ICommandAPIRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command> 
+            {
+                new Command { 
+                    Id = 0, HowTo = "Generate a migration", 
+                    Line = "dotnet ef migrations add <Name of Migration>", 
+                    Platform = ".NET Core EF" },
+                new Command { 
+                    Id = 1, HowTo = "Run Migrations", 
+                    Line = "dotnet ef database update", 
+                    Platform = ".NET Core EF" },
+                new Command { 
+                    Id = 2, HowTo = "List active migrations", 
+                    Line = "dotnet ef migrations list",
+                    Platform = ".NET Core EF" }
+            };
+
+            return commands; 
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { 
+                    Id = 0, HowTo = "Generate a migration", 
+                    Line = "dotnet ef migrations add <Name of Migration>", 
+                    Platform = ".NET Core EF" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
